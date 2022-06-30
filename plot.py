f7f969f99730e52cb301df5cc58b2e50fe942a7e
Võ Minh Hiếu <62029534+Picooooo@users.noreply.github.com>
2022-06-30 11:01:36 +0700

    Update plot.py

--- plot.py
+++ plot.py
@@ -61,3 +61,41 @@ def plot_one_experiments(log_folder, env_name, seed):
     plt.fill_between(x, (mean-ci), (mean+ci), color='blue', alpha=0.1)
     plt.legend()
     plt.savefig("./results/" + name + "/" + str(seed))
+   
+def plot(td3_folder, sac_folder, ddpg_folder, env_name):
+    folders = [td3_folder, sac_folder, ddpg_folder]
+    names = ["P3S-TD3", "P3S-SAC", "P3S-DDPG"]
+    colors = ["blue", "red", "green"]
+    index = 0
+    #loop through each algo
+    for log_folder in folders:
+        dir = os.listdir(log_folder)
+        list_folder = []
+        df = pd.DataFrame()
+        newpath = ''
+        for d in dir:
+            if os.path.isdir(os.path.join(log_folder, d)):
+                list_folder.append(d)
+
+        num_exp = len(list_folder)
+        for i in range(num_exp):
+            newpath = log_folder + list_folder[i] + '/progress.csv'
+            print(newpath)
+            df.insert(i,i,pd.read_csv(newpath)['return-average'][:1000])
+
+        mean = []
+        std = []
+        x = []
+        epoch = df.shape[0]
+        for i in range(epoch):
+            x.append(i*4000)
+            mean.append(df.iloc[i].sum()/num_exp)
+            std.append(np.std(df.iloc[i]))
+
+        ci = 1.96 * np.array(std)/np.sqrt(epoch)
+        plt.plot(x, mean, label=names[index])
+        plt.fill_between(x, (mean-ci), (mean+ci), color=colors[index], alpha=0.1)
+        index +=1
+    name = str(env_name) + "_3algo"
+    plt.title(str(name))
+    plt.legend()
